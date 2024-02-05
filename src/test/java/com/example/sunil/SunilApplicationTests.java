f4333a618019dbff753b253b722a0ace8711caa4
sunil <sunilyadav770275@gmail.com>
2024-02-05 16:24:57 +0530

    first commit

---
+++ src/test/java/com/example/sunil/SunilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sunil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
